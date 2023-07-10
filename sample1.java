c00472e5a9bf1a547f09f51f4f0d65c1f173321e
Luis Coco Enríquez <LEnriquez@luxoft.com>
2023-07-10 11:04:52 +0200

    first commit
    first commit

---
+++ sample1.java
@@ -0,0 +1,7 @@
+/** This is my first application in Java */
+public class sample1 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+}
